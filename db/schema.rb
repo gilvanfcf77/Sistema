0bb397ae61ab65667cd879167483787e8922fb12
Gilvan Ferreira <gilvanfcf77@gmail.com>
2016-09-08 19:07:18 -0300

    migrado de sqlite3 para pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160908204944) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "produtos", force: :cascade do |t|
     t.string   "name"
     t.string   "price"
