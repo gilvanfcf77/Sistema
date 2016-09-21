73789f75df4d1f8ef6d8a6de57581de0475e71ad
GUILHERME <guylherme.alves14@gmail.com>
2016-09-21 16:31:50 -0300

    edicao de texto e reordenacao

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -28,7 +28,7 @@ class ProdutosController < ApplicationController
 
     respond_to do |format|
       if @produto.save
-        format.html { redirect_to @produto, notice: 'Produto was successfully created.' }
+        format.html { redirect_to @produto, notice: 'Produto cadastrado com sucesso!' }
         format.json { render :show, status: :created, location: @produto }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ProdutosController < ApplicationController
   def update
     respond_to do |format|
       if @produto.update(produto_params)
-        format.html { redirect_to @produto, notice: 'Produto was successfully updated.' }
+        format.html { redirect_to @produto, notice: 'Produto atualizado com sucesso!.' }
         format.json { render :show, status: :ok, location: @produto }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ProdutosController < ApplicationController
   def destroy
     @produto.destroy
     respond_to do |format|
-      format.html { redirect_to produtos_url, notice: 'Produto was successfully destroyed.' }
+      format.html { redirect_to produtos_url, notice: 'Produto deletado com sucesso!' }
       format.json { head :no_content }
     end
   end
