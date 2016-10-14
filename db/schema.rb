6db2c7b4ca9792f8fc5896e5900f05f8d9407a95
gilvanfcf77 <gilvanfcf77@gmail.com>
2016-10-14 18:08:07 -0300

    Forms alteados

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013171509) do
+ActiveRecord::Schema.define(version: 20161014205844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20161013171509) do
 
   create_table "produtos", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
+    t.float    "price"
     t.text     "description"
     t.integer  "amount"
     t.string   "picture"
@@ -54,5 +54,14 @@ ActiveRecord::Schema.define(version: 20161013171509) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vendas", force: :cascade do |t|
+    t.string   "produto"
+    t.integer  "quantidade"
+    t.integer  "codigo"
+    t.date     "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
