a78d73c124f23feb15acf9de75a7b002afe0a759
gilvanfcf77 <gilvanfcf77@gmail.com>
2016-11-04 15:49:31 -0300

    user_notifier alterado

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,4 @@
 class UserNotifier < ApplicationMailer
-  class UserNotifier < ActionMailer::Base
   default :from => 'any_from_address@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
